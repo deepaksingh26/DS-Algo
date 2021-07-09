98a14117cea0731a94e64ee5c73aed1427933dc0
deepaksingh26 <deepakthakurnita@gmail.com>
2021-07-09 11:00:54 +0530

    data

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,83 +2,125 @@
 exportt default[
     {
         id:'1',
-        title:'Array'
+        title:'Array',
+        total question:36,
+        img:()
+
     },
     {
         id:'2',
         title:'Math'
+          total question:36,
+        img:()
     },
     {
         id:'3',
         title:'Searching'
+          total question:36,
+        img:()
     },
     {
         id:'4',
         title:'Sorting'
+          total question:36,
+        img:()
     },
     {
         id:'5',
         title:'Matrix'
+          total question:36,
+        img:()
     },
     {
         id:'6',
         title:'String'
+          total question:36,
+        img:()
     },
     {
         id:'7',
         title:'Hashing'
+          total question:36,
+        img:()
     },
     {
         id:'8',
         title:'Bit Masking'
+          total question:36,
+        img:()
     },
     {
         id:'9',
         title:'Linked List'
+          total question:36,
+        img:()
     },
     {
         id:'10',
         title:'Stack'
+          total question:36,
+        img:()
     },
     {
         id:'11',
         title:'Queue'
+          total question:36,
+        img:()
     },
     {
         id:'12',
         title:'Heap'
+          total question:36,
+        img:()
     },
     {
         id:'13',
         title:'Binary Tree'
+          total question:36,
+        img:()
     },
     {
         id:'14',
         title:'Binary Search Tree'
+          total question:36,
+        img:()
     },
     {
         id:'15',
         title:'Graph'
+          total question:36,
+        img:()
     },
     {
         id:'16',
         title:'Greedy'
+          total question:36,
+        img:()
     },
     {
         id:'17',
         title:'Dynamic Programing'
+          total question:36,
+        img:()
+
     },
     {
         id:'18',
         title:'Recursion'
+          total question:36,
+        img:()
     },
     {
         id:'19',
         title:'Algorithms'
+          total question:36,
+        img:()
     },
     {
         id:'20',
         title:'Design Patterns'
+          total question:36,
+        img:()
     },
 
 ]
